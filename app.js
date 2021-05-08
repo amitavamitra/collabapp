319358eae42ed2d0034b84900f0c76fbdb70dfba
amitavamitra <amitava.mitra@hotmail.co.uk>
2021-05-08 10:19:40 +0100

    2nd commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine' , 'ejs');
+
+app.get('/' , function(req,res){
+    res.render('home');
+})
+
+app.listen(3000, function(){
+    console.log('App running at port 3000');
+})
+
 

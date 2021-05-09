3bee29f0433cc165d5173032d354b20960b1de84
amitavamitra <amitava.mitra@hotmail.co.uk>
2021-05-09 21:14:44 +0100

    removing node-rfc dependencies for heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const https = require('https');
 const request = require('request');
-const rfcClient = require("node-rfc").Client; 
+// const rfcClient = require("node-rfc").Client; 
 require('dotenv').config();
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
@@ -133,79 +133,7 @@ socket.on('typing', (data)=>{
 
 
 app.post('/s4',function(req,res){
-// On user approval - call S/4 via node-rfc
-console.log('post method called')
-  const abapConnection = {
-    dest:'QJ9',
-    user:process.env.UQJ9,
-    passwd:process.env.PWD,
-    ashost: process.env.AHOST1,
-    sysnr: "00",
-    client: "500",
-  };
-  
-   
-  // create new client
-  const client = new rfcClient(abapConnection);
-
-  // echo SAP NW RFC SDK and nodejs/RFC binding version
-  console.log("Client version: ", client.version);
-  
-  // open connection
-  client.connect(function(err) {
-    
-    if (err) {
-      return console.error("could not connect to server",err);
-    } else {
-      console.log('Logged on to System:' + abapConnection.dest + " client " +abapConnection.client)
-    }
-  
-    var timest = new Date().getUTCMinutes();
-  
-  //   // invoke ABAP function module, passing structure and table parameters
-       
-      const headdata = {
-      MATERIAL        :       'text2ent' + timest,
-      IND_SECTOR      :       "M",
-      MATL_TYPE       :       "HALB",
-      BASIC_VIEW      :       "X",
-      MATERIAL_LONG   :      'text2ent' + timest
-          };
-  const clientdata  = {
-      OLD_MAT_NO     :   'abcd',
-      BASE_UOM       :    "EA",
-      MATL_GROUP     :    "01"  
-      };
-  const clientdatax  = {
-      OLD_MAT_NO     :    "X",
-      BASE_UOM       :    "X" ,
-      MATL_GROUP     :    "X"    
-      };
-  
-  const    MATERIALDESCRIPTION = [{
-  
-      LANGU        :         "EN",
-      LANGU_ISO    :          "EN",
-      MATL_DESC    :        'abcd'
-  
-  }];
-  
-          client.invoke(
-          "BAPI_MATERIAL_SAVEDATA",
-          { HEADDATA: headdata ,
-            CLIENTDATA: clientdata,
-            CLIENTDATAX:clientdatax,
-            MATERIALDESCRIPTION: MATERIALDESCRIPTION
-           },
-          function(err, res) {
-              if (err) {
-                  return console.error("Error invoking STFC_STRUCTURE:", err);
-              }
-              console.log("BAPI_MATERIAL_SAVEDATA call result:", res);
-          }
-      );
-      
-  });  // Dont comment this one..
+// would not work in heroku.
 });
 
 const PORT = process.env.PORT || 3000;
